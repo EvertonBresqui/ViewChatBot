7e3b89c777eebe07efec75a029a146f6089161bd
Aluno <Aluno@LF103M013-69333>
2020-02-15 14:17:40 -0300

    commit inicial

---
+++ Controllers/BotController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Chatbot.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Chatbot.Controllers
+{
+    public class BotController : Controller
+    {
+
+        private readonly IHttpContextAccessor _httpContextAccessor = null;
+
+        public BotController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public ActionResult Index()
+        {
+          
+ 
+            ViewBag.SessionId = Guid.NewGuid().ToString();
+
+            return View();
+        }
+
+
+    }
+}
